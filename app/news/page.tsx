05689ca45e85db78ba5371952d92bda05697374a
mettyeung <mettyeung9@gmail.com>
2025-10-30 04:53:19 +0700

    Update page.tsx

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -37,7 +37,7 @@ export default function NewsPage() {
 		(async () => {
 			setLoading(true);
 			const [postRes, catRes] = await Promise.all([
-				listBlogsService({ sort: "-publishedAt", limit: 24 }),
+				listBlogsService({ sort: "-publishedAt", limit: 100 }),
 				listCategoriesService(),
 			]);
 			if (postRes.status_code === 200 && postRes.data?.data) {
